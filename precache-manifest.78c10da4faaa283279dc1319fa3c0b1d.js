d529f528110cdf552612aa67c77f02b19588e9fa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-03 04:14:21 +0000

    Deploy ianmitch90/todo to github.com/ianmitch90/todo.git:gh-pages

---
+++ precache-manifest.78c10da4faaa283279dc1319fa3c0b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f113f9d7f4993b5202b4453969aeeb",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c9def3b43fba1b2f5ea8",
+    "url": "/todo/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "c3c998c60eb875155054",
+    "url": "/todo/static/js/2.bcea4370.chunk.js"
+  },
+  {
+    "revision": "eac0e0284d5461e5b08e9c3a7208f4cc",
+    "url": "/todo/static/js/2.bcea4370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9def3b43fba1b2f5ea8",
+    "url": "/todo/static/js/main.1ad97926.chunk.js"
+  },
+  {
+    "revision": "6e6ff19ef6de79be1e75",
+    "url": "/todo/static/js/runtime-main.89c2b063.js"
+  }
+]);
